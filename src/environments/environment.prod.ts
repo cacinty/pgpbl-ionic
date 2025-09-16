1a07c5e935b591e2be701d22c9650d8f592eb7eb
cacinty <cacacintiyabela@gmail.com>
2025-09-16 21:49:29 +0700

    Acara 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+  apiKey: "AIzaSyBr4FaUD7CP4SqPctt8pRBoX9peF9thICk",
+  authDomain: "pgpbl-ionic-66400.firebaseapp.com",
+  projectId: "pgpbl-ionic-66400",
+  storageBucket: "pgpbl-ionic-66400.firebasestorage.app",
+  messagingSenderId: "802267799971",
+  appId: "1:802267799971:web:1d320dd804f1a9579d3a16"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBr4FaUD7CP4SqPctt8pRBoX9peF9thICk",
+    authDomain: "pgpbl-ionic-66400.firebaseapp.com",
+    projectId: "pgpbl-ionic-66400",
+    storageBucket: "pgpbl-ionic-66400.firebasestorage.app",
+    messagingSenderId: "802267799971",
+    appId: "1:802267799971:web:1d320dd804f1a9579d3a16"
+  }
 };
 
 /*
